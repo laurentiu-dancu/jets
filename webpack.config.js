aaeb70f14ebf8e0898866033b441cf1e08dedd6f
unknown <laue.dancu@gmail.com>
2017-10-23 21:11:58 +0300

    Add build management plugin and templating.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -27,5 +29,12 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['build']),
+    new HtmlWebpackPlugin({
+      title: 'jets | novaFTL',
+      template: 'src/templates/index.ejs'
+    })
+  ]
 };
